e0882fa4876e5742fa525d522d26a97079d0dde6
Lucas Zanella <me@lucaszanella.com>
2019-06-30 17:21:59 -0300

    removed things

--- docker/run.sh
+++
@@ -1,6 +0,0 @@
-USER_=$USER
-#Used to intercept a package so I can edit live while testing. 
-#Then it's just a matter of sending to github and rebuilding the docker with no cache.
-INSERT_LOCAL="-v /home/$USER_/python-rtsp-client:/home/deps/python-rtsp-client"
-INSERT_LOCAL=
-xhost local:root && docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/$USER_/orwell:/home/dev/orwell $(echo $INSERT_LOCAL) -e DISPLAY=unix$DISPLAY orwell && xhost -
--- myRtspClient/third_party/JRTPLIB/src/rtptypes.h
+++
@@ -1,5 +0,0 @@
-#include "rtpconfig.h"
-
-
-#include <stdint.h>
-#include <sys/types.h>
